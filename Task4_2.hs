59859a17917ad59d66f98cd49928f670dedf067e
mazenomran <43556368+mazenomran@users.noreply.github.com>
2019-03-01 00:06:19 +0300

    task 4-2

--- Task4_2.hs
+++ Task4_2.hs
@@ -5,3 +5,17 @@ data FourOf a = FourOf a a a a deriving(Show,Eq)
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FourOf`
 -- таким образом, что 
 -- do { x <- FourOf 1 2 3 4; y <- FourOf 4 6 7 8; return $ x + y } === FourOf 5 8 10 12
+instance Functor FourOf where
+    fmap f (FourOf a b c d) = FourOf (f a) (f b) (f c) (f d)
+
+instance Applicative FourOf where
+    pure a = FourOf a a a a
+    (<*>) (FourOf a1 b1 c1 d1) (FourOf a2 b2 c2 d2) = FourOf (a1 a2) (b1 b2) (c1 c2) (d1 d2)
+
+instance Monad FourOf where
+    return a = FourOf a a a a
+    (>>=) (FourOf a b c d) f = FourOf (one (f a)) (two (f b)) (three (f c)) (four (f d)) where
+        one   (FourOf a _ _ _) = a
+        two   (FourOf _ b _ _) = b
+        three (FourOf _ _ c _) = c
+        four  (FourOf _ _ _ d) = d
