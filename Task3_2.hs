c347f1962bd471537afdc95fde1e832954438efb
mazenomran <43556368+mazenomran@users.noreply.github.com>
2019-03-01 00:02:43 +0300

    task 3-2

--- Task3_2.hs
+++ Task3_2.hs
@@ -5,9 +5,42 @@ import Todo(todo)
 data ReverseList a = RNil | RCons (ReverseList a) a
 
 rlistToList :: ReverseList a -> [a]
-rlistToList = todo
+rlistToList RNil               = []
+rlistToList (RCons prev value) = (rlistToList prev) ++ [value]
 
 listToRList :: [a] -> ReverseList a
-listToRList = todo
+listToRList []    = RNil
+listToRList (h:t) = RCons (listToRList t) h
 
--- Реализуйте классы Eq, Ord, Show, Monoid, Functor
+instance (Eq a) => Eq (ReverseList a) where
+    (==) RNil RNil                                 = True
+    (==) RNil _                                    = False
+    (==) _ RNil                                    = False
+    (==) (RCons prev1 value1) (RCons prev2 value2) = prev1 == prev2 && value2 == value2
+
+instance (Ord a) => Ord (ReverseList a) where
+    (<=) RNil RNil                                 = True
+    (<=) RNil _                                    = True
+    (<=) _ RNil                                    = False
+    (<=) (RCons prev1 value1) (RCons prev2 value2) = value1 <= value2 || prev1 <= prev2
+
+instance (Show a) => Show (ReverseList a) where
+    show RNil               = "[]"
+    show (RCons RNil value) = show value
+    show (RCons prev value) = show value ++ ", " ++ show prev
+
+instance Semigroup (ReverseList a) where
+    (<>) = mappend
+
+instance Monoid (ReverseList a) where
+    mempty = RNil
+
+    mappend RNil list               = list
+    mappend list RNil               = list
+    mappend list (RCons prev value) = RCons (mappend list prev) value
+
+    mconcat = foldr mappend mempty
+
+instance Functor ReverseList where
+    fmap _ RNil                      = RNil
+    fmap function (RCons prev value) = RCons (fmap function prev) (function value)
