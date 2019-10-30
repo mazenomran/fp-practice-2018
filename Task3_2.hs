839dacb6cc71d51df121d4573d6a6af24af70e1e
mazenomran <43556368+mazenomran@users.noreply.github.com>
2019-10-31 01:48:32 +0300

    task 3-2

--- Task3_2.hs
+++ Task3_2.hs
@@ -5,42 +5,44 @@ import Todo(todo)
 data ReverseList a = RNil | RCons (ReverseList a) a
 
 rlistToList :: ReverseList a -> [a]
-rlistToList RNil               = []
-rlistToList (RCons prev value) = (rlistToList prev) ++ [value]
+rlistToList RNil = []
+rlistToList (RCons xs x) = x : rlistToList xs 
+
 
 listToRList :: [a] -> ReverseList a
-listToRList []    = RNil
-listToRList (h:t) = RCons (listToRList t) h
+listToRList [] = RNil 
+listToRList list = foldl (\ revList elem -> RCons revList elem) RNil list
+
+-- Реализуйте классы Eq, Ord, Show, Monoid, Functor
+
 
 instance (Eq a) => Eq (ReverseList a) where
-    (==) RNil RNil                                 = True
-    (==) RNil _                                    = False
-    (==) _ RNil                                    = False
-    (==) (RCons prev1 value1) (RCons prev2 value2) = prev1 == prev2 && value2 == value2
+    (==) RNil RNil = True
+    (==) RNil _    = False
+    (==) _ RNil    = False
+    (==) argl argr = rlistToList argl == rlistToList argr
 
 instance (Ord a) => Ord (ReverseList a) where
-    (<=) RNil RNil                                 = True
-    (<=) RNil _                                    = True
-    (<=) _ RNil                                    = False
-    (<=) (RCons prev1 value1) (RCons prev2 value2) = value1 <= value2 || prev1 <= prev2
+    (<=) RNil RNil     = True
+    (<=) RNil _        = True
+    (<=) _ RNil        = False
+    (<=) argl argr = rlistToList argl <= rlistToList argr
 
 instance (Show a) => Show (ReverseList a) where
-    show RNil               = "[]"
-    show (RCons RNil value) = show value
-    show (RCons prev value) = show value ++ ", " ++ show prev
+    show RNil = "[]"
+    show (RCons RNil end) = show end
+    show (RCons start end) = show start ++ "," ++ show end
 
 instance Semigroup (ReverseList a) where
     (<>) = mappend
 
 instance Monoid (ReverseList a) where
-    mempty = RNil
-
-    mappend RNil list               = list
-    mappend list RNil               = list
-    mappend list (RCons prev value) = RCons (mappend list prev) value
-
-    mconcat = foldr mappend mempty
+    mempty = RNil    
+    mappend RNil arg = arg
+    mappend arg RNil = arg
+    mappend argl argr =
+        foldl (\ revList elem -> RCons revList elem) argl (rlistToList argr)
 
 instance Functor ReverseList where
-    fmap _ RNil                      = RNil
-    fmap function (RCons prev value) = RCons (fmap function prev) (function value)
+    fmap _ RNil = RNil
+    fmap arg (RCons start end) = RCons (fmap arg start) (arg end)
