6d49e651aa2fd97fee1e8107db9da4a61fa746b4
mazenomran <43556368+mazenomran@users.noreply.github.com>
2019-10-31 15:54:39 +0300

    task 5-2

--- Task5_2.hs
+++ Task5_2.hs
@@ -31,8 +31,18 @@ instance Monad Stream where
     return x = srepeat x
     ss >>= f = sflatten (f <$> ss)
 
+macSin :: Double -> Int -> Double
+macSin x 0 = x
+macSin x n = macSin x (n-1) + (-1)^n * x^(2*n + 1)/(fromIntegral $ product [1..(2*n + 1)])
+
 sinPrecisions :: Double -> Stream Double
-sinPrecisions = todo
+sinPrecisions x = r x 0 where
+  r x n = Cons (macSin x n) (r x (n+1))
+
+brothersE 0 = 2
+brothersE n = brothersE (n-1) + (2*n + 2) / (product [1..(2*n + 1)])
 
 ePrecisions :: Stream Rational
-ePrecisions = todo
+ePrecisions = r 0 where
+  r n = Cons (brothersE n) (r (n+1))
+
