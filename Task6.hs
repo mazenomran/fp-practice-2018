fc09feb95f2573846f1c082c73762a9466bfb425
mazenomran <43556368+mazenomran@users.noreply.github.com>
2019-10-31 22:30:07 +0300

    task 6

--- Task6.hs
+++ Task6.hs
@@ -2,11 +2,48 @@ module Task6 where
 
 import Todo(todo)
 
-data LinkedTree a = ChangeMe
 
-find :: LinkedTree a -> a -> Bool
-find = todo
-insert :: LinkedTree a -> a -> LinkedTree a
-insert = todo
+-- a binary search tree
+data LinkedTree a = Leaf { parent :: LinkedTree a }
+  | Node { value :: a -- payload
+         , left :: LinkedTree a -- left subtree
+         , right :: LinkedTree a -- right subtree
+         , parent :: LinkedTree a -- parent subtree
+         }
+
+-- searches for an element in a search tree
+find :: Ord a => a -> LinkedTree a -> Maybe (LinkedTree a)
+find _ (Leaf _) = Nothing
+find v' n@(Node v l r _) =
+  case compare v' v of
+    EQ -> Just n
+    LT -> find v' l
+    GT -> find v' r
+-- inserts an element into a binary search tree
+insert :: Ord a => a -> LinkedTree a -> LinkedTree a
+insert v' (Leaf parent) = 
+  let result = Node v' (Leaf result) (Leaf result) parent
+  in result
+insert v' n@(Node v l r p) = 
+  case compare v' v of
+    EQ -> n
+    LT -> let inserted = insert v' l
+              result = Node v inserted r p
+          in result
+    GT -> let inserted = insert v' r
+              result = Node v l inserted p
+          in result
+
+-- an examplary tree for testing purposes
+{-
+     5
+    /  \
+  3     8 
+ / \   / \
+1   4 6   10
+ \     \  /
+  2     7 9
+-}
+
 remove :: LinkedTree a -> a -> LinkedTree a
 remove = todo
